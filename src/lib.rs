b09e99bc5f2bedc318b76f5ecf2c62b0d3acab09
shunta0213 <shuntaide16213@gmail.com>
2024-04-07 00:10:18 +0900

    draw rhb

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
+use std::rc::Rc;
+use std::sync::Mutex;
+
 // prelude means '*' import allowed, it does not pollute namespace
-use rand::prelude::*;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 
@@ -16,60 +18,6 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 pub fn main_js() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
 
-    fn draw_triangle(
-        context: &web_sys::CanvasRenderingContext2d,
-        points: [(f64, f64); 3],
-        color: (u8, u8, u8),
-    ) {
-        let [top, left, right] = points;
-
-        let color_str = format!("rgb({},{},{})", color.0, color.1, color.2);
-        context.set_fill_style(&wasm_bindgen::JsValue::from_str(&color_str));
-
-        context.move_to(top.0, left.0);
-        context.begin_path();
-        context.line_to(left.0, left.1);
-        context.line_to(right.0, right.1);
-        context.line_to(top.0, top.1);
-        context.close_path();
-        context.stroke();
-        context.fill();
-    }
-
-    fn mid_point(point_1: (f64, f64), point_2: (f64, f64)) -> (f64, f64) {
-        return ((point_1.0 + point_2.0) / 2.0, (point_1.1 + point_2.1) / 2.0);
-    }
-
-    fn sierpinski(
-        context: &web_sys::CanvasRenderingContext2d,
-        points: [(f64, f64); 3],
-        color: (u8, u8, u8),
-        depth: u8,
-    ) {
-        draw_triangle(context, points, color);
-        let depth = depth - 1;
-
-        if depth > 0 {
-            let mut rng = thread_rng();
-
-            let [top, left, right] = points;
-            let left_mid = mid_point(top, left);
-            let right_mid = mid_point(top, right);
-            let bottom_mid = mid_point(left, right);
-            let next_color = (
-                rng.gen_range(0..255),
-                rng.gen_range(0..255),
-                rng.gen_range(0..255),
-            );
-
-            sierpinski(context, [top, left_mid, right_mid], next_color, depth);
-            sierpinski(context, [left_mid, left, bottom_mid], next_color, depth);
-            sierpinski(context, [right_mid, bottom_mid, right], next_color, depth);
-        }
-    }
-
-    // document can be optional in javascript (at least theoretically)
-    // so web_sys::window() returns option<Window>
     // if window is None, the game cannot be start so unwrap is enough
     let window = web_sys::window().unwrap();
     let document = window.document().unwrap();
@@ -91,8 +39,38 @@ pub fn main_js() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    let first_points = [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)];
-    sierpinski(&context, first_points, (255, 255, 255), 10);
+    wasm_bindgen_futures::spawn_local(async move {
+        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
+        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
+        let error_tx = Rc::clone(&success_tx);
+
+        // draw image
+        let image = web_sys::HtmlImageElement::new().unwrap();
+        let callback = Closure::once(move || {
+            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut x| x.take()) {
+                success_tx.send(Ok(()));
+            }
+        });
+        let error_callback = Closure::once(move |err| {
+            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut x| x.take()) {
+                web_sys::console::log_1(&JsValue::from_str(&format!(
+                    "Error loading image: {:?}",
+                    err
+                )));
+                error_tx.send(Err(err));
+            }
+        });
+
+        image.set_src("Idle (1).png");
+        image.set_onload(Some((callback.as_ref()).unchecked_ref()));
+        image.set_onerror(Some((error_callback.as_ref()).unchecked_ref()));
+        // after end of this scope, callback will be dropped without calling forget()
+        // intentionally cause memory leak
+        callback.forget();
+
+        success_rx.await;
+        context.draw_image_with_html_image_element(&image, 100.0, 100.0);
+    });
 
     Ok(())
 }
